3cbe3e6de099c9e5bb2106ada1ab772226ead9fe
Chris Woelk <chris@redbeed.com>
2021-02-27 16:21:46 +0100

    Fix icons width

--- resources/views/home/components/frontpage-teaser.blade.php
+++ resources/views/home/components/frontpage-teaser.blade.php
@@ -1,6 +1,8 @@
 <div class="bg-white rounded-xl px-6 py-4 shadow-lg" data-aos="fade-up" data-aos-delay="{{ $fadeDelay ?? 0 }}">
     <div class="flex items-center h-full">
-        <i class="{{ $icon }} text-5xl text-flamingo-800 mr-6"></i>
+        <i class="text-5xl text-flamingo-800 mr-6">
+            <i class="{{ $icon }} fa-fw"></i>
+        </i>
         <div>
             <h2 class="font-extrabold text-3xl mb-2">
                 {!! __('home.teaser.'.$translationKey.'.title') !!}
